cc33a0733c6f028a4f399409509890007d9d98ce
Amal Ajith <pmamalster@gmail.com>
2022-03-10 19:05:24 +0530

    Integrated commander cli
    ## Integrated commander cli to generate cli helpers.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Wordpress Gutenberg Block Editor PO file to JED1.X JSON
+
+Generate Wordpress Gutenberg block editor compatible JED1.x JSON 
+file from the PO files.
+
+This npm package is a utility wrapper package built around
+the po2json npm package. 
--- cli.js
+++ cli.js
@@ -1,13 +1,26 @@
 #!/usr/bin/env node
 
-var path = require('path');
-var fs = require('fs');
+const path = require('path');
+const fs = require('fs');
+const {program} = require("commander");
+const wordpressPo2Json = require("./build");
+const version = require('./package.json').version;
 
-global.appRoot = path.resolve(process.cwd());
+program.version(version)
+    .description("Generate JED1.x compatible JSON files from PO files for use in internationalization of Wordpress Gutenberg block.")
+    .option('-lf --language-folder [languageFolder]', 'Language folder location with the po files, relative to the package.json.')
+    .option('-wjh --wordpress-js-handle [ wpJsHandleName ]', 'The handle used to refer the bundled JS of the Gutenberg block')
+    .action(function (options) {
+        console.log(options);
 
-console.log(appRoot);
+        global.appRoot = path.resolve(process.cwd());
 
-const languagesFolder = `${appRoot}/languages`
+        console.log(appRoot);
 
-const wordpressPo2Json = require('./build');
-wordpressPo2Json.generatePoJson({ languagesFolderLocation: languagesFolder });
+        const languagesFolder = options.languageFolder ? `${appRoot}/${options.languageFolder}` : `${appRoot}/languages`;
+        const wpJsHandleName = options.wpJsHandleName ? options.wpJsHandleName : null;
+        const wordpressPo2Json = require('./build');
+        wordpressPo2Json.generatePoJson({languagesFolderLocation: languagesFolder, wordpressJsHandleName: wpJsHandleName});
+
+    })
+    .parse();
